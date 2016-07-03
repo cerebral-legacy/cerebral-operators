7743de1d2871ccf7d513dab3d2475730133cd2f7
Garth Williams <garth@wi.llia.ms>
2016-07-03 13:19:44 +0200

    Updated when tests to use latest syntax and use cerebral signals for testing

--- test/when.js
+++ test/when.js
@@ -1,180 +1,141 @@
 /*global beforeEach,afterEach,describe,it*/
 import when from '../src/when'
 import { reset, check, expect, expectCount } from './helpers/chaiCounter'
+import controller from './helpers/controller'
+
+controller.addSignals({
+  whenTestTrue: {
+    chain: [
+      when('trueTest'), {
+        true: [ () => { expect(true).to.be.ok } ],
+        false: []
+      }
+    ],
+    immediate: true
+  },
+  whenTestFalse: {
+    chain: [
+      when('falseTest'), {
+        true: [],
+        false: [ () => { expect(true).to.be.ok } ]
+      }
+    ],
+    immediate: true
+  },
+  whenTestInputTrue: {
+    chain: [
+      when('input:test'), {
+        true: [ () => { expect(true).to.be.ok } ],
+        false: []
+      }
+    ],
+    immediate: true
+  },
+  whenTestInputFalse: {
+    chain: [
+      when('input:test'), {
+        true: [],
+        false: [ () => { expect(true).to.be.ok } ]
+      }
+    ],
+    immediate: true
+  },
+  whenTestCustomTrue: {
+    chain: [
+      when('trueTest', { yes: true, no: when.otherwise }), {
+        yes: [ () => { expect(true).to.be.ok } ],
+        no: []
+      }
+    ],
+    immediate: true
+  },
+  whenTestCustomFalse: {
+    chain: [
+      when('falseTest', { yes: true, no: when.otherwise }), {
+        yes: [],
+        no: [ () => { expect(true).to.be.ok } ]
+      }
+    ],
+    immediate: true
+  },
+  whenTestImplicitOtherwise: {
+    chain: [
+      when('falseTest', { yes: true }), {
+        yes: [],
+        otherwise: [ () => { expect(true).to.be.ok } ]
+      }
+    ],
+    immediate: true
+  },
+  whenTestArray: {
+    chain: [
+      when('yesTest', [ 'yes', 'no' ]), {
+        yes: [ () => { expect(true).to.be.ok } ],
+        no: [],
+        otherwise: []
+      }
+    ],
+    immediate: true
+  }
+})
+
+const signals = controller.getSignals()
+let tree
 
 beforeEach(reset)
 afterEach(check)
 
 describe('when()', function () {
-  it('should call true when true', function () {
-    expectCount(2)
-
-    const action = when('test')
-
-    action({
-      state: {
-        get (path) {
-          expect(path).to.equal('test')
-          return true
-        }
-      },
-      output: {
-        true () {
-          expect(true).to.be.ok
-        },
-        false () {
-        }
-      }
+  beforeEach(function () {
+    tree = controller.model.tree
+    tree.set({
+      trueTest: true,
+      falseTest: false,
+      yesTest: 'yes'
     })
+    tree.commit()
+  })
+
+  it('should call true when true', function () {
+    expectCount(1)
+    signals.whenTestTrue()
   })
 
   it('should call true when input is true', function () {
     expectCount(1)
-
-    const action = when('input:test')
-
-    action({
-      input: {
-        test: true
-      },
-      output: {
-        true () {
-          expect(true).to.be.ok
-        },
-        false () {
-        }
-      }
+    signals.whenTestInputTrue({
+      test: true
     })
   })
 
   it('should call false when false', function () {
-    expectCount(2)
-
-    const action = when('test')
-
-    action({
-      state: {
-        get (path) {
-          expect(path).to.equal('test')
-          return false
-        }
-      },
-      output: {
-        true () {
-        },
-        false () {
-          expect(true).to.be.ok
-        }
-      }
-    })
+    expectCount(1)
+    signals.whenTestFalse()
   })
 
   it('should call false when input is false', function () {
-    expectCount(1)
-
-    const action = when('input:test')
-
-    action({
-      input: {
-        test: false
-      },
-      output: {
-        true () {
-        },
-        false () {
-          expect(true).to.be.ok
-        }
-      }
+    signals.whenTestInputFalse({
+      test: false
     })
   })
 
   it('should call customTrue when true', function () {
-    expectCount(2)
-
-    const action = when('test', { yes: true, no: when.otherwise })
-
-    action({
-      state: {
-        get (path) {
-          expect(path).to.equal('test')
-          return true
-        }
-      },
-      output: {
-        yes () {
-          expect(true).to.be.ok
-        },
-        no () {
-        }
-      }
-    })
+    expectCount(1)
+    signals.whenTestCustomTrue()
   })
 
   it('should call customFalse when false', function () {
-    expectCount(2)
-
-    const action = when('test', { yes: true, no: when.otherwise })
-
-    action({
-      state: {
-        get (path) {
-          expect(path).to.equal('test')
-          return false
-        }
-      },
-      output: {
-        yes () {
-        },
-        no () {
-          expect(true).to.be.ok
-        }
-      }
-    })
+    expectCount(1)
+    signals.whenTestCustomFalse()
   })
 
   it('should call implicitally add otherwise when not supplied', function () {
-    expectCount(2)
-
-    const action = when('test', { yes: true })
-
-    action({
-      state: {
-        get (path) {
-          expect(path).to.equal('test')
-          return false
-        }
-      },
-      output: {
-        yes () {
-        },
-        otherwise () {
-          expect(true).to.be.ok
-        }
-      }
-    })
+    expectCount(1)
+    signals.whenTestImplicitOtherwise()
   })
 
   it('should accept an array of conditions', function () {
-    expectCount(2)
-
-    const action = when('test', [ 'yes', 'no' ])
-
-    action({
-      state: {
-        get (path) {
-          expect(path).to.equal('test')
-          return 'no'
-        }
-      },
-      output: {
-        yes () {
-        },
-        no () {
-          expect(true).to.be.ok
-        },
-        otherwise () {
-        }
-      }
-    })
+    expectCount(1)
+    signals.whenTestArray()
   })
 })
